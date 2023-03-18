f6e4f52bbdfde75e18ce6ead7c65586f0bf5ec12
fluffysheepinx <fluffysheepinx@gmail.com>
2023-03-19 06:51:18 +0900

    プロジェクト ファイルを追加します。

---
+++ mapJ/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace mapJ
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MapGenerator _mapGenerator;
+        public int Scale { get; set; } = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            MapCanvas.MouseWheel += grdCanvas_MouseWheel;
+            _mapGenerator = new MapGenerator(MapCanvas, hexSize:60);
+        }
+
+        private void grdCanvas_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if ((Keyboard.IsKeyDown(Key.LeftCtrl) == false) && (Keyboard.IsKeyDown(Key.RightCtrl) == false))
+            {
+                return;
+            }
+
+            ScaleTransform? scaleTransform = null;
+            if (MapCanvas.RenderTransform == null)
+            {
+                scaleTransform = new ScaleTransform();
+            }
+            else
+            {
+                var tran = MapCanvas.RenderTransform as ScaleTransform;
+                if (tran != null)
+                {
+                    scaleTransform = tran;
+                }
+                else
+                {
+                    scaleTransform = new ScaleTransform();
+                }
+            }
+
+            double scale = 0;
+            if (this.Scale > e.Delta)
+            {
+                this.Scale = this.Scale - 1;
+                scale = scaleTransform.ScaleX - 0.1;
+            }
+            else if (this.Scale == e.Delta)
+            {
+                return;
+            }
+            else
+            {
+                this.Scale = this.Scale + 1;
+                scale = scaleTransform.ScaleX + 0.1;
+            }
+
+            // 倍率を制限する（大きいとドラッグでバグることがあるけど、原因はよく分からず）
+            if (scale > 2.0)
+            {
+                scale = 2.0; // 最大 200%
+            }
+            if (scale < 0.1)
+            {
+                scale = 0.1; // 最小 50%
+            }
+            scaleTransform.ScaleX = scale;
+            scaleTransform.ScaleY = scale;
+            MapCanvas.RenderTransform = scaleTransform; ;
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MapCanvas.Children.Clear();
+
+            string[] mapChipFiles = { "10005.png", "10010.png", "10006.png" };
+
+            int minLineCount = 20;
+            int maxLineCount = 20;
+
+            _mapGenerator.GenerateFantasyMap(mapChipFiles, minLineCount, maxLineCount);
+        }
+    }
+}
---
+++ mapJ/MapGenerator.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace mapJ
+{
+    public class MapGenerator
+    {
+        private double _hexSize;
+        private double _hexRadius;
+        private double _hexGap;
+        private readonly Canvas _canvas;
+        private readonly Random _random;
+
+        public MapGenerator(Canvas canvas, double hexSize)
+        {
+            _canvas = canvas;
+            _hexSize = hexSize;
+            _random = new Random();
+            _hexRadius = GetHexRadius(_hexSize);
+            _hexGap = GetHexGap(_hexSize);
+        }
+        #region MyRegion
+        private double GetHexRadius(double size)
+        {
+            return size / 2;
+        }
+        #endregion
+        #region MyRegion
+        private double GetHexGap(double size)
+        {
+            return size / 4;
+        }
+        #endregion
+        #region MyRegion
+        public void GenerateMapChips(string[] mapChipFiles)
+        {
+            foreach (var file in mapChipFiles)
+            {
+                var bitmap = new BitmapImage(new Uri("pack://application:,,,/Resources/" + file, UriKind.Absolute));
+                var image = new Image { Source = bitmap, Height = 32, Width = 32 };
+
+                // ランダムな座標に画像を配置する
+                var x = _random.Next((int)_canvas.ActualWidth - (int)image.Width);
+                var y = _random.Next((int)_canvas.ActualHeight - (int)image.Height);
+                Canvas.SetLeft(image, x);
+                Canvas.SetTop(image, y);
+
+                _canvas.Children.Add(image);
+            }
+        }
+        #endregion
+
+        public void GenerateHexMap(int rowCount, int colCount)
+        {
+            double hexHeight = GetHexHeight(_hexRadius);
+            double hexWidth = GetHexWidth(_hexRadius);
+
+            Hexagon hexagon = new Hexagon(_hexRadius);
+
+            for (int row = 0; row < rowCount; row++)
+            {
+                for (int col = 0; col < colCount; col++)
+                {
+                    double x = col * hexWidth * 3 / 4;
+                    double y = row * (hexHeight + _hexGap);
+                    if (col % 2 == 1)
+                    {
+                        y += hexHeight / 2 + _hexGap / 2;
+                    }
+
+                    var path = new Path
+                    {
+                        Stroke = Brushes.Black,
+                        StrokeThickness = 1
+                    };
+
+                    var geometry = new PathGeometry();
+                    var figure = new PathFigure
+                    {
+                        StartPoint = hexagon.GetPoint(0, x, y)
+                    };
+                    for (int i = 1; i <= 6; i++)
+                    {
+                        figure.Segments.Add(new LineSegment(hexagon.GetPoint(i, x, y), true));
+                    }
+                    geometry.Figures.Add(figure);
+                    path.Data = geometry;
+
+                    _canvas.Children.Add(path);
+                }
+            }
+        }
+
+        public class Hexagon
+        {
+            private readonly double _radius;
+            private readonly double _height;
+            private readonly double _width;
+
+            public Hexagon(double radius)
+            {
+                _radius = radius;
+                _height = 2 * radius;
+                _width = Math.Sqrt(3) * radius;
+            }
+
+            public System.Windows.Point GetPoint(int index, double x, double y)
+            {
+                double angle = 2.0 * Math.PI / 6 * index;
+                double x1 = _radius * Math.Cos(angle);
+                double y1 = _radius * Math.Sin(angle);
+
+                double absX = x + _width / 2 + x1;
+                double absY = y + _height / 2 + y1;
+
+                return new System.Windows.Point(absX, absY);
+            }
+        }
+        #region MyRegion
+        private double GetHexHeight(double radius)
+        {
+            return Math.Sqrt(3) * radius;
+        }
+        #endregion
+        #region MyRegion
+        private double GetHexWidth(double radius)
+        {
+            return 2 * radius;
+        }
+        #endregion
+
+        public void GenerateFantasyMap(string[] mapChipFiles, int minLineCount, int maxLineCount)
+        {
+            // 六角形のマスを描画する
+            GenerateHexMap(minLineCount, maxLineCount);
+
+            // マップチップを配置する
+            GenerateMapChips(mapChipFiles);
+        }
+    }
+}
